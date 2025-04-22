9a1ef6d9e6893081ebf87e224a36c686b0d50a84
Keval Sompura <kimlerrr>
2025-04-22 18:04:05 -0400

    Mongodb Atlas support

--- backend/config/settings.js
+++ backend/config/settings.js
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const mongoConfig = {
-  serverUrl: "mongodb://localhost:27017/",
-  database: "StudentBridge",
+  serverUrl: process.env.MONGO_SERVER_URL || "mongodb://localhost:27017/",
+  database: process.env.MONGO_DB_NAME || "StudentBridge",
+  imgBucket: "uploads",
 };
+export const sessionSecret =
+  process.env.SESSION_SECRET || "your_session_secret_here";
+("");
